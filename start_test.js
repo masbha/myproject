ca00e83b96dab253e85f356fb03bcfd618835bd7
Masbha Uddin Ahmed <masbha2001@gmail.com>
2013-12-18 12:50:38 +0000

    remove some method from this file
    This is for test

--- start_test.js
+++ start_test.js
@@ -1,10 +1,3 @@
-var target = UIATarget.localTarget();
-var app = target.frontMostApp();
-var window = app.mainWindow();
-var host = target.host();
-
-
-
 function inArray(needle, haystack) {
     for (var key in haystack) {
         if (needle === haystack[key]) {
@@ -12,26 +5,4 @@ function inArray(needle, haystack) {
         }
     }
     return false;
-}
-
-function capitaliseFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function arrayKeyExist(needle, haystack) {
-    for (var key in haystack) {
-        if (needle === key) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function getArrayKey(needle, haystack) {
-    for (var key in haystack) {
-        if (needle === haystack[key]) {
-            return key;
-        }
-    }
-    return -1;
 }
